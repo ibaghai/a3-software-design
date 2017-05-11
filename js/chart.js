a1b79ad968e666f844d5df4fbdde64832587cca9
NickNordale <nicknordale@gmail.com>
2017-05-11 01:22:14 -0700

    Done with method creation & testing

--- js/chart.js
+++ js/chart.js
@@ -1,17 +1,24 @@
 function chart() {
     var width = 960,
-        height = 600;
+        height = 600,
+        nodeSize = "r",
+        nodeSizeMultiplier = 10,
+        linkSize = "weight",
+        linkSizeMultiplier = 1.5,
+        linkStrengthMultiplier = 100,
+        nodeAttraction = -4;
 
     function my(selection) {
 
         selection.each(function(data) {
+
             var simulation = d3.forceSimulation()
                 .force("link", d3.forceLink().id(function (d) {
                     return d.id;
                 }))
-                .force("charge", d3.forceManyBody().strength(-400))
-                .force("y", d3.forceY(height / 2))
-                .force("x", d3.forceX(width / 2));
+                .force("linkStrength",  function(d) { return d[linkSize] * linkStrengthMultiplier; } )
+                .force("charge", d3.forceManyBody().strength(nodeAttraction * 100))
+                .force("center", d3.forceCenter(width / 2, height / 2));
 
             var svg = d3.select(this).selectAll("svg").data([data]);
 
@@ -26,18 +33,21 @@ function chart() {
                 d.target = d.target_id;
             });
 
+            console.log(linkSizeMultiplier);
+            console.log(nodeAttraction);
+
             var link = gEnter.append("g")
                 .style("stroke", "#aaa")
                 .selectAll("line")
                 .data(data.links)
-                .enter().append("line");
+                .enter().append("line")
+                    .attr("stroke-width", function(d) { return d[linkSize] * linkSizeMultiplier; });
 
             var node = gEnter.append("g")
                 .attr("class", "nodes")
                 .selectAll("circle")
                 .data(data.nodes)
                 .enter().append("circle")
-                .attr("r", 6)
                 .call(d3.drag()
                     .on("start", dragstarted)
                     .on("drag", dragged)
@@ -53,7 +63,6 @@ function chart() {
                     return d.name;
                 });
 
-
             simulation
                 .nodes(data.nodes)
                 .on("tick", ticked);
@@ -63,24 +72,29 @@ function chart() {
                 .links(data.links);
 
             function ticked() {
+                simulation
+                    .force("charge", d3.forceManyBody().strength(nodeAttraction * 100))
+                    .force("linkStrength",  function(d) { return d[linkSize] * linkStrengthMultiplier; } );
+
                 link
-                    .attr("x1", function(d) { return d.source.x - 20; })
-                    .attr("y1", function(d) { return d.source.y - 20; })
-                    .attr("x2", function(d) { return d.target.x - 20; })
-                    .attr("y2", function(d) { return d.target.y - 20; });
+                    .attr("x1", function(d) { return d.source.x; })
+                    .attr("y1", function(d) { return d.source.y; })
+                    .attr("x2", function(d) { return d.target.x; })
+                    .attr("y2", function(d) { return d.target.y; })
+                    .attr("stroke-width", function(d) { return d[linkSize] * linkSizeMultiplier; });
 
                 node
-                    .attr("r", 20)
+                    .attr("r", function(d) { return d[nodeSize] * nodeSizeMultiplier; })
                     .style("fill", "#d9d9d9")
                     .style("stroke", "#969696")
                     .style("stroke-width", "1px")
-                    .attr("cx", function (d) { return d.x - 20; })
-                    .attr("cy", function(d) { return d.y - 20; });
+                    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
                 label
-                    .attr("x", function(d) { return d.x - 28; })
-                    .attr("y", function(d) { return d.y - 12; })
-                    .style("font-size", "20px").style("fill", "#4393c3");
+                    .attr("x", function(d) { return d.x - (((nodeSizeMultiplier / 2) - (nodeSizeMultiplier / 10)) * d[nodeSize]); })
+                    .attr("y", function(d) { return d.y + (((nodeSizeMultiplier / 2) - (nodeSizeMultiplier / 10)) * d[nodeSize]); })
+                    .style("font-size", function(d) { return d[nodeSize] * nodeSizeMultiplier + "px"; })
+                    .style("fill", "#4393c3");
             }
 
             function dragstarted(d) {
@@ -102,15 +116,39 @@ function chart() {
         });
     }
 
-    my.width = function(value) {
-        if (!arguments.length) return width;
-        width = value;
+    my.nodeSize = function(value) {
+        if (!arguments.length) return nodeSize;
+        nodeSize = value;
         return my;
     };
 
-    my.height = function(value) {
-        if (!arguments.length) return height;
-        height = value;
+    my.nodeSizeMultiplier = function(value) {
+        if (!arguments.length) return nodeSizeMultiplier;
+        nodeSizeMultiplier = value;
+        return my;
+    };
+
+    my.linkSize = function(value) {
+        if (!arguments.length) return linkSize;
+        linkSize = value;
+        return my;
+    };
+
+    my.linkSizeMultiplier = function(value) {
+        if (!arguments.length) return linkSizeMultiplier;
+        linkSizeMultiplier = value;
+        return my;
+    };
+
+    my.linkStrengthMultiplier = function(value) {
+        if (!arguments.length) return linkStrengthMultiplier;
+        linkStrengthMultiplier = value;
+        return my;
+    };
+
+    my.nodeAttraction = function(value) {
+        if (!arguments.length) return nodeAttraction;
+        nodeAttraction = value;
         return my;
     };
 
